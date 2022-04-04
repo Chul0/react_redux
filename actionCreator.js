2f825d346b0122cdcf72a9b6199f750369d2c54a
Sungchul Park <sungchulpark0@gmail.com>
2022-04-04 18:12:42 -0400

    reducers

--- actionCreator.js
+++ actionCreator.js
@@ -30,3 +30,28 @@ const createClaim = (name, amountOfMoneyToCollect) => {
     }
   };
 };
+
+//Reducers (Departments!)
+const claimHistory = (oldListOfClaims = [], action) => { //Reducers don't know what the data(arguments) are when it's first called(undefined) – you need to default the values - e.g., give it an empty array, integers, etc
+    if (action.type === 'CREATE_CLAIM') {
+      //we care about this action (FORM!)
+      return [...oldListOfClaims, action.payload]
+      //joining the two elements into a new array
+      //spread operator copies the whole array and store it in a new array.
+      
+      //we don't care the action (form!)
+    }
+    //we don't care the action (form!)
+    return oldListOfClaims;
+  };
+  
+  const accounting = (bagOfMoney = 100, action) => {
+    if (action.type === 'CREATE_CLAIM') {
+      return bagOfMoney - action.payload.amoutOfMoneyToCollect;
+    } else if (action.type === 'CRATE_POLICY') {
+      return bagOfMoney + action.payload.amount;
+    }
+    
+    return bagOfMoney;
+    
+  };
