586ea696c5d752510b1fe1d2f4562c22be169c5e
Sungchul Park <sungchulpark0@gmail.com>
2022-04-04 17:55:02 -0400

    initial commit

---
+++ actionCreator.js
@@ -0,0 +1,32 @@
+console.clear();
+
+// People droping off a form (Action creator)
+
+const createPolicy = (name, amount) => {
+  return { //Action (a form in our analogy)
+    type: 'CREATE_POLICY',
+    payload: {
+      name: name,
+      amount: amount
+    }
+  };
+};
+
+const deletePolicy = (name) => {
+  return {
+    type: 'DELETE_POLICY',
+    payload: {
+      name: name
+    }
+  };
+};
+
+const createClaim = (name, amountOfMoneyToCollect) => {
+  return {
+    type: 'CREATE_CLAIM',
+    payload: {
+      name: name,
+      amountOfMoneyToCollect: amountOfMoneyToCollect
+    }
+  };
+};
