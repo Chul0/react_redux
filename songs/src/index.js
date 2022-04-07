9199ea0f1733faebcdc161ca4a11c42bf32744c5
Sungchul Park <sungchulpark0@gmail.com>
2022-04-07 19:56:13 -0400

    Wire reducers to Provider

--- songs/src/index.js
+++ songs/src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'; //Provider should be imported from react-redux, not redux(not using Context!)
+import { createStore } from 'redux'; //Store is a single object that combines Reducers and Action Creators
 import App from './components/App';
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App /> 
+    </Provider>, document.querySelector('#root')
+    );
