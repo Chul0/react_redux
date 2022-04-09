a8118ec421fbad530b3ebe2708b57b4718886ac4
Sungchul Park <sungchulpark0@gmail.com>
2022-04-09 15:01:47 -0400

    Configured connect

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -3,11 +3,20 @@ import { connect } from 'react-redux'; //you need to use connect directly in the
 
 class SongList extends Component{
     render() {
+        console.log(this.props);
         return <div>SongList</div>;
     }
 }
 
-export default connect()(SongList);
+const mapStateToProps = (state) => {
+    //this function is named mapStateToProps by convention:Take a state object and map it and convert it to props
+    //state should have all the reducers
+    console.log('state', state)
+    return { songs: state.songs} //identical to this.props { songs: state.songs}
+}
+
+export default connect(mapStateToProps)(SongList);
+//^You need to pass states from Reducers to connect - so you get state from Provider as props
 
 
 
