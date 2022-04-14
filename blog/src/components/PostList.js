02ac8e6521df71d2b3a335ac3d19ad0bf2a8bf1a
Sungchul Park <sungchulpark0@gmail.com>
2022-04-13 22:39:11 -0400

    Created action creator; wired it to cmp

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+    
     render () {
         return <div>Post List</div>;
     }
 };
 
-export default PostList;
+export default connect(
+    null, //this should always be mapStateToProp eventually. null is fine as an initial setup
+    { fetchPosts }
+)(PostList);
