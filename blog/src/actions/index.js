805f962f32128928d1adb59bdccf16490d83925e
Sungchul Park <sungchulpark0@gmail.com>
2022-04-18 21:42:44 -0400

    Hooking up redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,5 +1,13 @@
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
 export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POST'
-    };
+    return function(dispatch, getState) {
+        const promise = jsonPlaceholder.get('/posts');
+        //without async..await, we can get promise object – synchronous
+    
+        return {
+            type: 'FETCH_POST',
+            payload: promise
+        };
+    }
 };
