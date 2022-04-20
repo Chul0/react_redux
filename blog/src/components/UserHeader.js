b24a47085ee8097e4d973e1d8ce08bf5419b6f05
Sungchul Park <sungchulpark0@gmail.com>
2022-04-19 23:23:46 -0400

    Refactoring; second prop:ownProps to mapStateToProps

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -9,18 +9,24 @@ class UserHeader extends React.Component {
     }
 
     render() {
-        const user = this.props.users.find((user) => user.id === this.props.userId);
+        // const user = this.props.users.find((user) => user.id === this.props.userId);
+        const { user } = this.props;
 
         if(!user) {
             return null;
             //when this cmp is first loaded, it will have an empty [] as it's set in usersReducer.
         }
+
         return <div className='header'>{user.name}</div>;
     }
 }
 
-const mapStateToProps = (state) => {
-    return { users: state.users}
-}
+const mapStateToProps = (state, ownProps) => {
+    return { user: state.users.find(user => user.id === ownProps.userId ) };
+    //mapStateToProps has a second argument:ownProps that refers to the props of this cmp
+};
 
-export default connect(mapStateToProps, { fetchUser })(UserHeader);
+export default connect(
+    mapStateToProps, 
+    { fetchUser }
+)(UserHeader);
