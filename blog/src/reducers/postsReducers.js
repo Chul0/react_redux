f784c1eac90b113249e4b43254e577742cee9870
Sungchul Park <sungchulpark0@gmail.com>
2022-04-19 22:00:41 -0400

    postsReducers setup

--- blog/src/reducers/postsReducers.js
+++ blog/src/reducers/postsReducers.js
@@ -1,3 +1,18 @@
-export default () => {
-    return 123;
+export default (state = [], action) => {
+    if(action.type === 'FETCH_POSTS') {
+        return action.payload;
+    }
+
+    return state;
+    //if the action 'FETCH_POSTS' is not called, just return the state you have previously.
 };
+
+
+/* Another way with switch statement.
+switch (action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
+*/
