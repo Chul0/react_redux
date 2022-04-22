f8eb363e2093119d07769262af9b146d5d6d16a4
Sungchul Park <sungchulpark0@gmail.com>
2022-04-22 12:37:29 -0400

    installed/imported lodash to memoize

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,11 @@
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = () => async dispatch =>  {
         const response = await jsonPlaceholder.get('/posts');
         
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
+         //instead of returning an object, call dispatch and pass object because I'm using redux-thunk
     };
 
 
@@ -12,4 +14,6 @@ export const fetchUser = id => async dispatch => {
 
     dispatch({ type: 'FETCH_USER', payload: response.data});
 };
+//Since we don't have any variable.. you can omit `return`, move async dispatch right next to function name.
+//Defining a funciton that is going to return a funtion
 
